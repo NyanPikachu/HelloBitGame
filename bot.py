ed99efd610d3090b6d7a09b160a89d93972f1960
NyanPikachu <32774211+NyanPikachu@users.noreply.github.com>
2018-04-24 00:28:34 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,6 +25,7 @@ async def save_prefix(prefix, guildID):
 @bot.event
 async def on_ready():
     print("Bot is online!")
+    await bot.change_presence(activity=discord.Activity(name=f'BattleBit stats! | !help', type=discord.ActivityType.playing))
     
 @bot.command()
 async def ping(ctx):
@@ -52,30 +53,5 @@ async def prefix(ctx, prefix=None):
     except Exception as e:
         await ctx.send(f'Something went wrong\nError Log: `str({e})`')
     
-@bot.command(name='presence')
-@bot.is_owner
-async def _presence(ctx, type=None, *, game=None):
-    '''Change the bot's presence'''
-    if type is None:
-        await ctx.send(f'Usage: `{ctx.prefix}presence [game/stream/watch/listen] [message]`')
-    else:
-        if type.lower() == 'stream':
-            await bot.change_presence(activity=discord.Activity(name=game, type=discord.ActivityType.streaming))
-            await ctx.send(f'Set presence to. `Streaming {game}`')
-        elif type.lower() == 'game':
-            await bot.change_presence(activity=discord.Activity(name=game, type=discord.ActivityType.playing))
-            await ctx.send(f'Set presence to `Playing {game}`')
-        elif type.lower() == 'watch':
-            await bot.change_presence(activity=discord.Activity(name=game, type=discord.ActivityType.watching))
-            await ctx.send(f'Set presence to `Watching {game}`')
-        elif type.lower() == 'listen':
-            await bot.change_presence(activity=discord.Activity(name=game, type=discord.ActivityType.listening))
-            await ctx.send(f'Set presence to `Listening to {game}`')
-        elif type.lower() == 'clear':
-            await bot.change_presence(activity=discord.Activity(name=None))
-            await ctx.send('Cleared Presence')
-        else:
-            await ctx.send('Usage: `.presence [game/stream/watch/listen] [message]`')
- 
-    
+
 bot.run(os.environ.get("TOKEN"))
